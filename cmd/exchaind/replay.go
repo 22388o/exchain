b80d901d8eb01cc1fb948d5c153f2bbbefd14c94
chunfengSun <516108736@qq.com>
2021-11-18 16:57:54 +0800

    delete unless code

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -274,9 +274,6 @@ func doReplay(ctx *server.Context, state sm.State, stateStoreDB dbm.DB,
 		if needSaveBlock {
 			SaveBlock(ctx, originBlockStore, height)
 		}
-		if height >= lastBlockHeight+10 {
-			break
-		}
 	}
 }
 
